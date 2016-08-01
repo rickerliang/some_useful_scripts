c670e37bd5fc60b9da4ea5a6253eee1187e35129
rickerliang <rickerliang@gmail.com>
2016-08-01 14:27:49 +0800

    添加注释

--- refine.sh
+++ refine.sh
@@ -8,12 +8,12 @@ git_user_name=$(git config user.name)
 # 捕捉xml内file路径，到coding层
 origin_coding_dir=$(cat $1 -v |  grep -oPm1 "(?<=<error file=\").+(?=\" line=\")" | grep -ioP ".+coding")
 origin_coding_dir=${origin_coding_dir//\\/\\\\}
-# 替换为./, 例如./api/etapi/kde/kkdeservice.h
+# 替换为./, 例如coding/abc/123/test.h 替换为./abc/123/test.h
 replace_coding_dir="."
 replace_coding_dir=${replace_coding_dir//\\/\\\\}
 echo "git user name $git_user_name"
 
-# 替换为./，例如./api/etapi/kde/kkdeservice.h
+# 替换为./, 例如coding/abc/123/test.h 替换为./abc/123/test.h
 sed "s/$origin_coding_dir/$replace_coding_dir/g" $1 > __tmp1.xml
 # 针对window的路径斜杠
 sed 's/\\/\//g' __tmp1.xml > __tmp2.xml
