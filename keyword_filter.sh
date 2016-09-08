df17208a03b728da676a052c11d4cd03fff8cdca
rickerliang <rickerliang@gmail.com>
2016-09-08 23:03:51 +0800

    Update keyword_filter.sh

--- keyword_filter.sh
+++ keyword_filter.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+mkdir -p filter_out
+
 while read p; do
   find "$2" -type f -maxdepth 1 -name "*$p*" -exec bash -c '
     echo -n -e "\033[Kmoving $1\r"
-    mv "$1" filter_out/
-  ' bash {} \;
+    mkdir -p "filter_out/$2"
+    mv "$1" "filter_out/$2"
+  ' bash {} "$p" \;
 done < "$1"
